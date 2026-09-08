f287579bc878911b05a0f9da43a77680696c22a0
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-09-08 10:17:07 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,13 @@
 __name__ = "Rename-Bot"
 __version__ = "3.1.0"
 __license__ = " Apache License, Version 2.0"
-__copyright__ = "Copyright (C) 2022-present Digital Botz <https://github.com/DigitalBotz>"
-__programer__ = "<a href=https://github.com/TechifyBots/Rename-Bot>Techify Bots</a>"
+__copyright__ = "Copyright (C) 2022-present Rename Botz <https://t.me/Hari_Moviez>"
+__programer__ = "<a href=https://t.me/Hari_Moviez>Hari_Moviez</a>"
 __library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
 __language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
 __database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
-__developer__ = "<a href=https://t.me/TechifyBots>Techify Bots</a>"
-__maindeveloper__ = "<a href=https://t.me/Digital_Botz>Digital Botz</a>"
+__developer__ = "<a href=https://t.me/Harikushal>Hari Kushal</a>"
+__maindeveloper__ = "<a href=https://t.me/TG_BOTS_UPDATE>HK Channel</a>"
 
 
 from pyrogram import Client, filters
