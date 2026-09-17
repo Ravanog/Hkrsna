d24acc9ff20cd7419231e444af08460183ad793d
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-09-17 19:57:40 +0530

    Update start_and_cb.py

--- plugins/start_and_cb.py
+++ plugins/start_and_cb.py
@@ -331,27 +331,28 @@ async def cb_handler(client, query: CallbackQuery):
 			],[
              InlineKeyboardButton("ʙᴀᴄᴋ", callback_data = "help")]])) 
 		
-	elif data == "audio_tool":
+    elif data == "caption":
+        await query.message.edit_text(
+            text=rkn.CAPTION,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[
+             InlineKeyboardButton("ʙᴀᴄᴋ", callback_data = "help")]]))
+
+	
+    elif data == "audio_tool":
         await query.message.edit_text(
             text=rkn.AUDIO_TOOL,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
              InlineKeyboardButton("ʙᴀᴄᴋ", callback_data = "harikushal")]])) 
 
-	elif data == "video_tool":
+    elif data == "video_tool":
         await query.message.edit_text(
             text=rkn.VIDEO_TOOL,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
              InlineKeyboardButton("ʙᴀᴄᴋ", callback_data = "harikushal")]])) 
-	
-
-    elif data == "caption":
-        await query.message.edit_text(
-            text=rkn.CAPTION,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton("ʙᴀᴄᴋ", callback_data = "help")]])) 
+      
       
     elif data == "custom_file_name":
         await query.message.edit_text(
